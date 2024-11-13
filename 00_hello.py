fa34a87cece2f14d8854a0740c18194c49081e53
Jason Dev <jasonssdev@gmail.com>
2024-11-13 12:31:55 -0500

    hello world with exercises

---
+++ 00_hello.py
@@ -0,0 +1,68 @@
+# Hello World Comment, this is my first print
+print("Hello, World")
+
+
+
+# 1. Imprime "Â¡Hola Mundo!" por consola.
+
+print("Hello, World")
+
+# 2. Escribe un comentario de una sola lÃ­nea explicando quÃ© hace el cÃ³digo del Ejercicio 1.
+
+# Print
+print("Hello, World")
+
+# 3. Imprime tu nombre y edad en la misma lÃ­nea utilizando la funciÃ³n print.
+
+print("Jason", 34)
+
+# 4. Usa la funciÃ³n type() para imprimir el tipo de dato de una cadena de texto, un nÃºmero entero y un nÃºmero decimal.
+
+# Datatype query
+print(type("Hello world")) # > <class 'str'>
+print(type(7)) # > <class 'int'>
+print(type(7.7)) # > <class 'float'>
+print(type(True)) # > <class 'Bool'>
+
+# 5. Escribe un comentario en varias lÃ­neas explicando quÃ© son los tipos de datos en Python.
+
+'''
+In Python, a datatype is a classification that specifies the type of value a variable can hold,
+such as integers (int), floating-point numbers (float), strings (str), lists (list),
+dictionaries (dict), and others.
+'''
+
+# 6. Imprime el resultado de concatenar dos cadenas de texto, por ejemplo: "Hola" y "Mundo".
+
+print("Hello" + " " + "World")
+
+# 7. Crea una variable para almacenar tu nombre, otra para tu edad, e imprime ambas en la misma lÃ­nea.
+
+name = "Jason"
+age = 34
+
+print(name, age)
+
+# 8. Escribe un programa que solicite al usuario su nombre y lo imprima junto con un saludo.
+
+#user_name = input("enter your name: ")
+#print("Hello", user_name)
+
+# 9. Usa print() para mostrar el resultado de la suma de dos nÃºmeros enteros y el tipo de dato resultante.
+
+print(3+4)
+print(type(3+4))
+
+# 10. Comenta el cÃ³digo del Ejercicio 9, y explica quÃ© hace cada lÃ­nea usando comentarios de una sola lÃ­nea.
+
+# Asign intengers in variables
+num_1 = 3
+num_2 = 4
+# sum integers
+result = num_1 + num_2
+# print results
+print(result)
+# print datatype
+print(type(result))
+
+
